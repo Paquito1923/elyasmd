ddf4313de1351f134e8b46adc62fe5b9f839552a
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-04-12 13:02:04 +0700

    Create _autoreact.js

---
+++ plugins/_autoreact.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, text }) => {
+  
+  conn.sendMessage(m.chat, {
+        react: {
+          text: `${pickRandom(['😨', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🤨','🥴','👆','😔', '👀','👎'])}`,
+          key: m.key,
+        }})
+  
+}
+
+handler.customPrefix = /^(sad|oh?|owh|idk|cum|crot|y|f|g|adick|banh|dek|turu|yntkts|ajg|kontol|ngocok|p)$/i
+handler.command = new RegExp
+
+handler.exp = 3
+
+module.exports = handler
+
+function pickRandom(list) {
+   return list[Math.floor(Math.random() * list.length)]
+}
