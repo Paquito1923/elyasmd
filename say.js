4d4d9f445265a959f294807771ae2515255817b5
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-06-23 23:01:50 +0700

    Delete say.js

--- say.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { text }) => {
-m.reply(text)
-}
-handler.help = ['say', 'katakan']
-handler.tags = ['info']
-handler.command = /^(say|katakan)$/i
-
-module.exports = handler
