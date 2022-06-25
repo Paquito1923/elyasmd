364dcdf4ded761e4c6ad2c235847475eccc94c7d
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-06-25 15:52:49 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,73 +1,25 @@
-const { sticker5, sticker1 } = require('../lib/sticker')
-
+const { sticker } = require('../lib/sticker')
 const uploadFile = require('../lib/uploadFile')
-
 const uploadImage = require('../lib/uploadImage')
-
-
-
+let { webp2png } = require('../lib/webp2mp4')
+let fetch = require("node-fetch")
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-  let stiker = false
-
-  try {
-
-    let [packname, ...author] = text.split`|`
-
-    author = (author || []).join`|`
-
     let q = m.quoted ? m.quoted : m
-
-    let mime = m.quoted.mimetype || ''
-
-    if (/webp/.test(mime)) {
-
+    let mime = (q.msg || q).mimetype || ''
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+try {
       let img = await q.download()
-
-      let out = await uploadFile(img)
-
-      stiker = await sticker5(0, out, packname || global.packname, author || global.author)
-
-    } else if (/image/.test(mime)) {
-
-      let img = await q.download()
-
-      let out = await uploadImage(img)
-
-      stiker = await sticker5(0, out, packname || global.packname, author || global.author)
-
-    } else if (/video/.test(mime)) {
-
-      if ((q.msg || q).seconds > 11) return m.reply('maks 10 detik!')
-
-      let img = await q.download()
-
-      let out = await uploadImage(img)
-
-      stiker = await sticker5(0, out, packname || global.packname, author || global.author)
-
-    }
-
-  } finally {
-
-    if (stiker) await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
-
-    else throw `Balas stiker dengan perintah *${usedPrefix + command} <teks>|<teks>*`
-
-  }
-
+   
+if(!img) throw `balas gambar/video/stiker dengan perintah .stiker` 
+conn.sendImageAsSticker(m.chat , img, m, {packname: text, author: ''})
+    } catch { throw 'Gagal!, Balas Gambar/video dengan caption *.stiker*'}
 }
-
-handler.help = ['wm <teks>|<teks>']
-
+handler.help = ['wm', 'watermark']
 handler.tags = ['sticker']
-
-handler.command = /^(wm)$/i
-
-
-
-handler.limit = true
-
-
+handler.command = /^wm|watermark?$/i
 
 module.exports = handler
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
